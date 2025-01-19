00082081b3f5e0a333288eab3ad24f82919500c0
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-01-20 01:09:32 +0200

    second commit

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODO's");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
-Console.WriteLine("Hello, World!");
+string userInput = Console.ReadLine();
+Console.WriteLine("User input: " + userInput);
+
+// string userInput = "A";
+// Console.WriteLine(userInput);
+//
+// userInput = "ABC";
+// Console.WriteLine(userInput);
+//
+// int number;
+// number = 6;
+//
+// Console.WriteLine(number);
+
+//
+// int a = 10;
+// int b = 25;
+// ++a;
+// --b;
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+//
+// Console.WriteLine("Addition: " + (a + b));
+// Console.WriteLine("Subtrsaction: " + (a - b));
+// Console.WriteLine("Multiplication: " + a * b);
+// Console.WriteLine("Division: " + a / b);
+// Console.WriteLine("john" + " " + "Smith");
+//
+// string word = "abc";
+// Console.WriteLine(word + a);
+//
+
+Console.ReadKey();
