d5461741452f437c962858401f4ddd4cdc968a0b
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-02-22 00:45:02 +0200

    added extra project

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -4,39 +4,39 @@ Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
-string userInput = Console.ReadLine();
-Console.WriteLine("User input: " + userInput);
-userInput = "ABC";
-
-var isUserInputAbc = userInput == "ABC";
-var isUserInputNotAbc = userInput != "ABC";
-var isUserInputNotAbc2 = !(userInput == "ABC");
-
-var number = 10;
-
-var isLargerThan5 = number > 5;
-var isSmallerThan10 = number < 10;
-var isLargerOrEqualTo10 = number >= 10;
-var isSmallerOrEqualTo6 = number <= 6;
-
-var isLargerThan4AndSmallerThan9 = number > 4 && number < 9;
-var isEqualTo2OrLargerThan6 = number == 2 || number == 6;
-
-if (userInput.Length <= 3)
-{
-    Console.WriteLine("User input is short");
-}
-else if (userInput.Length < 10)
-{
-    Console.WriteLine("User input is medium");
-}
-else
-{
-    Console.WriteLine("User input is long");
-}
-
- 
-Console.ReadKey();
+// string userInput = Console.ReadLine();
+// Console.WriteLine("User input: " + userInput);
+// userInput = "ABC";
+//
+// var isUserInputAbc = userInput == "ABC";
+// var isUserInputNotAbc = userInput != "ABC";
+// var isUserInputNotAbc2 = !(userInput == "ABC");
+//
+// var number = 10;
+//
+// var isLargerThan5 = number > 5;
+// var isSmallerThan10 = number < 10;
+// var isLargerOrEqualTo10 = number >= 10;
+// var isSmallerOrEqualTo6 = number <= 6;
+//
+// var isLargerThan4AndSmallerThan9 = number > 4 && number < 9;
+// var isEqualTo2OrLargerThan6 = number == 2 || number == 6;
+//
+// if (userInput.Length <= 3)
+// {
+//     Console.WriteLine("User input is short");
+// }
+// else if (userInput.Length < 10)
+// {
+//     Console.WriteLine("User input is medium");
+// }
+// else
+// {
+//     Console.WriteLine("User input is long");
+// }
+//
+//  
+// Console.ReadKey();
 // string userInput = "A";
 // Console.WriteLine(userInput);
 //
@@ -67,11 +67,21 @@ Console.ReadKey();
 //
 
 
-var userChoice = Console.ReadLine();
-bool isLong = IsLong(userChoice);
+// var userChoice = Console.ReadLine();
+// bool isLong = IsLong(userChoice);
+//
+// bool IsLong(string input)
+// {
+//     return input.length > 10;
+// }
 
 var result = Add(10, 5);
 Console.WriteLine("10 + 5 = " + result);
+
+Console.WriteLine("provide a number");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+Console.WriteLine(number);
 
 // // the below is example of poor quality code 
 // if (userChoice == "S")
